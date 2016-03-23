1ef1fd37439dd147513a4876121fc01716a79b1d
jc314116 <brandon.winship@my.jcu.edu.au>
2016-03-23 12:16:56 +1000

    Workshop 4 for week 5, changed file by adding my name and number to it.

--- basic_functions.py
+++ basic_functions.py
@@ -6,6 +6,7 @@
 CP1404/CP5632 Workshop 04
 Basic functions
 demonstrating various parameters, returns and the use of a main function
+Brandon Winship (13144404)
 """
 __author__ = 'Lindsay Ward'
 
@@ -18,9 +19,12 @@ def main():
 
 def get_limits():
     minimum = int(input("Enter the minimum: "))
-    maximum = int(input("Enter the maximum: "))
-    return minimum, maximum
-
+    maximum = int(input("Enter the maximum (10 or above): "))
+    while maximum > minimum:
+        print("Maximum too low!")
+        maximum = int(input("Enter the maximum (10 or above): "))
+    else:
+        return minimum, maximum
 
 def print_between(start, end):
     for i in range(start, end+1):
